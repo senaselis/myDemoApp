793c3f621b94eabf4172bce9551f91293ab5ee19
senaselis <senaselis@gmail.com>
2017-09-20 20:58:18 +0300

    new method implementation

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -38,22 +38,23 @@ public class AppTest
     }
 
     public void testFound() {
-      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertTrue(new App().search(array, 4));
+      String[] array = new String[]{"AppTest", "SoftwareEngineering", "GitHub", "AppTest"};
+      assertTrue(new App().search(array, "GitHub",1));
     }
 
     public void testNotFound() {
-      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertFalse(new App().search(array, 5));
+      String[] array = new String[]{"AppTest", "SoftwareEngineering", "GitHub"};
+      assertFalse(new App().search(array, "GitHub",2));
+      assertFalse(new App().search(array, "Java",1));
     }
 
     public void testEmptyArray() {
-      ArrayList<Integer> array = new ArrayList<>();
-      assertFalse(new App().search(array, 1));
+      String[] array = new String[]{};
+      assertFalse(new App().search(array, "GitHub",1));
     }
 
     public void testNull() {
-      assertFalse(new App().search(null, 1));
+      assertFalse(new App().search(null, "GitHub",1));
     }
 
 }
